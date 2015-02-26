1bdb65c68829d7c6e19463dc19bd940e05ced1cc
ChalkPE <amato0617@gmail.com>
2015-02-26 14:33:10 +0900

    문서화 주석 수정 및 코드 순서 재배치

--- Pool.js
+++ Pool.js
@@ -18,9 +18,7 @@
 
 /**
  * @since 2015-02-24
- * @author ChalkPE <amato0617@gmail.com> 
- * @author Choseul <chocoslime05@naver.com> 
- * @author IchiKaku <woni8708@naver.com> 
+ * @author ChalkPE <amato0617@gmail.com>
  */
 var Pool = {};
 
@@ -32,6 +30,9 @@ Pool.API_VERSION = 1;
 
 
 /**
+ * @since 2015-02-26
+ * @author onebone <yoonchul.jeong00@gmail.com>
+ * @author ChalkPE <amato0617@gmail.com>
  * @param path {String | java.io.File} 서브 모듈이 저장된 소스 파일의 경로 및 파일 객체
  */
 Pool.load = function(path){
@@ -44,13 +45,18 @@ Pool.load = function(path){
 };
 
 /**
+ * @since 2015-02-26
+ * @author ChalkPE <amato0617@gmail.com>
  * @param error {Error} 출력할 에러
  */
 Pool.showError = function(error){
 	clientMessage(error.name + ": " + error.message + " at line " + error.lineNumber + "\n" + error.stack);
 };
 
-
+/**
+ * @since 2015-02-26
+ * @author IchiKaku <woni8708@naver.com>
+ */
 Pool.Vector2 = function(x, y){
 	this.x = Math.floor(x);
 	this.y = Math.floor(y);
@@ -74,7 +80,10 @@ Pool.Vector2.prototype.set = function(x, y){
 	}
 };
 
-
+/**
+ * @since 2015-02-24
+ * @author ChalkPE <amato0617@gmail.com>
+ */
 Pool.Vector3 = function(x, y, z){
 	this.x = Math.floor(x);
 	this.y = Math.floor(y);
@@ -103,13 +112,18 @@ Pool.Vector3.prototype.set = function(x, y, z){
 };
 
 
-Pool.Canvas = {};
 
-Pool.IO = {};
 
-Pool.Entity = {};
 
 /**
+ * @since 2015-02-24
+ * @author ChalkPE <amato0617@gmail.com>
+ */
+Pool.Canvas = {};
+
+/**
+ * @since 2015-02-24
+ * @author ChalkPE <amato0617@gmail.com>
  * @param begin {Pool.Vector3} 시작점의 위치
  * @param end {Pool.Vector3} 종료점의 위치
  * @param blockId {int} 선을 이룰 블럭의 ID 값
@@ -117,106 +131,109 @@ Pool.Entity = {};
  * @see Bresenham's line algorithm
  */
 Pool.Canvas.drawLine = function(begin, end, blockId, blockDamage){
+	function drawLine2D(x0, y0, x1, y1, height, blockId, blockDamage){
+	    var dx = Math.abs(x1 - x0);
+	    var dy = Math.abs(y1 - y0);
+	    var sx = (x0 < x1) ? 1 : -1;
+	    var sy = (y0 < y1) ? 1 : -1;
+	    var err = dx - dy;
+	    
+	    while(true){
+	        Level.setTile(x0, height, y0, blockId, blockDamage);
+	        
+	        if((x0 == x1) && (y0 == y1)){
+	            break;
+	        }
+	        var e2 = 2 * err;
+	        if(e2 > -dy){
+	            err -= dy;
+	            x0 += sx;
+	        }
+	        if(e2 < dx){
+	            err += dx;
+	            y0 += sy;
+	        }
+	    }
+	}
+
+	function drawLine3D(x0, y0, z0, x1, y1, z1, blockId, blockDamage){
+	    var x0i = Math.floor(x0);
+	    var y0i = Math.floor(y0);
+	    var z0i = Math.floor(z0);
+	    
+	    var x1i = Math.floor(x1);
+	    var y1i = Math.floor(y1);
+	    var z1i = Math.floor(z1);
+	    
+	    var sx = (x1i > x0i) ? 1 : (x1i < x0i) ? -1 : 0;
+	    var sy = (y1i > y0i) ? 1 : (y1i < y0i) ? -1 : 0;
+	    var sz = (z1i > z0i) ? 1 : (z1i < z0i) ? -1 : 0;
+	    
+	    var x = x0i;
+	    var y = y0i;
+	    var z = z0i;
+	    
+	    var xp = x0i + (x1i > x0i ? 1 : 0);
+	    var yp = y0i + (y1i > y0i ? 1 : 0);
+	    var zp = z0i + (z1i > z0i ? 1 : 0);
+	    
+	    var vx = (x1 === x0) ? 1 : x1 - x0;
+	    var vy = (y1 === y0) ? 1 : y1 - y0;
+	    var vz = (z1 === z0) ? 1 : z1 - z0;
+	    
+	    var vxvy = vx * vy;
+	    var vxvz = vx * vz;
+	    var vyvz = vy * vz;
+	    
+	    var errx = (xp - x0) * vyvz;
+	    var erry = (yp - y0) * vxvz;
+	    var errz = (zp - z0) * vxvy;
+	    
+	    var derrx = sx * vyvz;
+	    var derry = sy * vxvz;
+	    var derrz = sz * vxvy;
+
+	    do{
+	        Level.setTile(x, y, z, blockId, blockDamage);
+	        
+	        if(x === x1i && y === y1i && z === z1i){
+	            break;
+	        }
+	        
+	        var xr = Math.abs(errx);
+	        var yr = Math.abs(erry);
+	        var zr = Math.abs(errz);
+	        
+	        if(sx !== 0 && (sy === 0 || xr < yr) && (sz === 0 || xr < zr)){
+	            x += sx;
+	            errx += derrx;
+	        }else if(sy !== 0 && (sz === 0 || yr < zr)){
+	            y += sy;
+	            erry += derry;
+	        }else if(sz !== 0){
+	            z += sz;
+	            errz += derrz;
+	        }
+	    }while(true);
+	}
+	
 	if(begin.equals(end)){ //Point
 		Level.setTile(begin.x, begin.y, begin.z, blockId, blockDamage);
 	}else if(begin.x === end.x){
-		Pool.Canvas.drawLine2D(begin.y, begin.z, end.y, end.z, begin.x, blockId, blockDamage);
+		drawLine2D(begin.y, begin.z, end.y, end.z, begin.x, blockId, blockDamage);
 	}else if(begin.y === end.y){
-		Pool.Canvas.drawLine2D(begin.x, begin.z, end.x, end.z, begin.y, blockId, blockDamage);
+		drawLine2D(begin.x, begin.z, end.x, end.z, begin.y, blockId, blockDamage);
 	}else if(begin.z === end.z){
-		Pool.Canvas.drawLine2D(begin.x, begin.y, end.x, end.y, begin.z, blockId, blockDamage);
+		drawLine2D(begin.x, begin.y, end.x, end.y, begin.z, blockId, blockDamage);
 	}else{
-		Pool.Canvas.drawLine3D(begin.x, begin.y, begin.z, end.x, end.y, end.z, blockId, blockDamage);
+		drawLine3D(begin.x, begin.y, begin.z, end.x, end.y, end.z, blockId, blockDamage);
 	}
 };
 
-Pool.Canvas.drawLine2D = function(x0, y0, x1, y1, height, blockId, blockDamage){
-    var dx = Math.abs(x1 - x0);
-    var dy = Math.abs(y1 - y0);
-    var sx = (x0 < x1) ? 1 : -1;
-    var sy = (y0 < y1) ? 1 : -1;
-    var err = dx - dy;
-    
-    while(true){
-        Level.setTile(x0, height, y0, blockId, blockDamage);
-        
-        if((x0 == x1) && (y0 == y1)){
-            break;
-        }
-        var e2 = 2 * err;
-        if(e2 > -dy){
-            err -= dy;
-            x0 += sx;
-        }
-        if(e2 < dx){
-            err += dx;
-            y0 += sy;
-        }
-    }
-}
-
-Pool.Canvas.drawLine3D = function(x0, y0, z0, x1, y1, z1, blockId, blockDamage){
-    var x0i = Math.floor(x0);
-    var y0i = Math.floor(y0);
-    var z0i = Math.floor(z0);
-    
-    var x1i = Math.floor(x1);
-    var y1i = Math.floor(y1);
-    var z1i = Math.floor(z1);
-    
-    var sx = (x1i > x0i) ? 1 : (x1i < x0i) ? -1 : 0;
-    var sy = (y1i > y0i) ? 1 : (y1i < y0i) ? -1 : 0;
-    var sz = (z1i > z0i) ? 1 : (z1i < z0i) ? -1 : 0;
-    
-    var x = x0i;
-    var y = y0i;
-    var z = z0i;
-    
-    var xp = x0i + (x1i > x0i ? 1 : 0);
-    var yp = y0i + (y1i > y0i ? 1 : 0);
-    var zp = z0i + (z1i > z0i ? 1 : 0);
-    
-    var vx = (x1 === x0) ? 1 : x1 - x0;
-    var vy = (y1 === y0) ? 1 : y1 - y0;
-    var vz = (z1 === z0) ? 1 : z1 - z0;
-    
-    var vxvy = vx * vy;
-    var vxvz = vx * vz;
-    var vyvz = vy * vz;
-    
-    var errx = (xp - x0) * vyvz;
-    var erry = (yp - y0) * vxvz;
-    var errz = (zp - z0) * vxvy;
-    
-    var derrx = sx * vyvz;
-    var derry = sy * vxvz;
-    var derrz = sz * vxvy;
-
-    do{
-        Level.setTile(x, y, z, blockId, blockDamage);
-        
-        if(x === x1i && y === y1i && z === z1i){
-            break;
-        }
-        
-        var xr = Math.abs(errx);
-        var yr = Math.abs(erry);
-        var zr = Math.abs(errz);
-        
-        if(sx !== 0 && (sy === 0 || xr < yr) && (sz === 0 || xr < zr)){
-            x += sx;
-            errx += derrx;
-        }else if(sy !== 0 && (sz === 0 || yr < zr)){
-            y += sy;
-            erry += derry;
-        }else if(sz !== 0){
-            z += sz;
-            errz += derrz;
-        }
-    }while(true);
-}
-
 /**
+ * @since 2015-02-24
+ * @author ChalkPE <amato0617@gmail.com>
+ * @author IchiKaku <woni8708@naver.com>
  * @param center {Pool.Vector3} 원의 중심
  */
 Pool.Canvas.drawCircle = function(center, radius, blockId, blockDamage){
@@ -229,11 +246,26 @@ Pool.Canvas.drawCircle = function(center, radius, blockId, blockDamage){
 };
 
 
+
+
+
+/**
+ * @since 2015-02-26
+ * @author Choseul <chocoslime05@naver.com>
+ */
+Pool.IO = {};
+
+/**
+ * @since 2015-02-26
+ * @author Choseul <chocoslime05@naver.com>
+ */
 Pool.IO.Zipper = function(kirito, psycho){
 	//TODO: Implement this method!
 }
 
 /**
+ * @since 2015-02-26
+ * @author affogatoman <colombia2@naver.com>
  * @param path {String | java.io.File} 문자열을 저장할 파일의 경로 및 파일 객체
  */
 Pool.IO.saveFile = function(path, str){
@@ -253,6 +285,8 @@ Pool.IO.saveFile = function(path, str){
 };
 
 /**
+ * @since 2015-02-26
+ * @author onebone <yoonchul.jeong00@gmail.com>
  * @param path {String | java.io.File} 문자열을 읽을 파일의 경로 및 파일 객체
  * @return {String} 파일 내의 문자열
  */
@@ -284,6 +318,8 @@ Pool.IO.readAllLines = function(path){
 };
 
 /**
+ * @since 2015-02-26
+ * @author ChalkPE <amato0617@gmail.com>
  * @param path {String | java.io.File} 지울 파일의 경로 및 파일 객체
  * @return {boolean} 파일 삭제 성공 여부
  */
@@ -301,7 +337,18 @@ Pool.IO.removeFile = function(path){
 };
 
 
+
+
+
 /**
+ * @since 2015-02-26
+ * @author netherTNT <canghaun@naver.com>
+ */
+Pool.Entity = {};
+
+/**
+ * @since 2015-02-26
+ * @author netherTNT <canghaun@naver.com>
  * @param range {int} 범위
  * @param baseEntity {int} 범위의 중심이 되는 엔티티의 엔티티 아이디 (기본값: 플레이어)
  * @returns {Array} 범위 내의 모든 엔티티
@@ -317,6 +364,14 @@ Pool.Entity.getEntitiesInRange = function(range, baseEntity){
 };
 
 
+
+
+
+/**
+ * @since 2015-02-26
+ * @author ChalkPE <amato0617@gmail.com>
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/hypot
+ */
 Math.hypot = Math.hypot || function(){
 	var y = 0;
 	for(var i = 0; i < arguments.length; i++){
@@ -328,6 +383,14 @@ Math.hypot = Math.hypot || function(){
 	return Math.sqrt(y);
 };
 
+
+
+
+
+/**
+ * @since 2015-02-24
+ * @author ChalkPE <amato0617@gmail.com>
+ */
 function selectLevelHook(){
 	var scripts = net.zhuoweizhang.mcpelauncher.ScriptManager.scripts;
     var ScriptableObject = org.mozilla.javascript.ScriptableObject;
@@ -339,4 +402,3 @@ function selectLevelHook(){
 		}
 	}
 }
-
