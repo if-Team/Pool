5bf1dc9b8b2d1c193f4c1cb2af1c9d54e9684e5f
우유맛비누 <milk5590@naver.com>
2015-03-07 20:25:50 +0900

    버그 또 수정

--- Pool.js
+++ Pool.js
@@ -1165,7 +1165,7 @@ Pool.Player.removeItem = function(id, damage, count){
 	for(var cc = 9; cc <= 44; cc++){
 		if(Player.getInventorySlot(cc) === id && Player.getInventorySlotData(cc) === damage){
 			var k = Player.getInventorySlotCount(cc);
-			Player.addItemInventory(id, -real, damage);
+			Player.addItemInventory(id, -Math.min(k, real), damage);
 			real -= k;
 			if(cc <= 0) return true;
 		}
@@ -1426,7 +1426,7 @@ Pool.Math.random = function(a){
  * @author CodeInside <scgtdy7151@gmail.com>
  */
 Pool.Math.randomPM = function(){
-	return (Math.random() >= 0.5 ? 1 : -1);
+	Math.random() >= 0.5 ? return 1 : return -1;
 };
 
 /**
@@ -1457,14 +1457,14 @@ Pool.Math.avgArray = function(array){
 };
 
 /**
- * 두수의 기하평균을 구합니다
+ * 두수의 산술기하평균을 구합니다
  * 
  * @since 2015-03-05 (API 1)
  * @author CodeInside <scgtdy7151@gmail.com>
  * @param {Number} a
  * @param {Number} b
  */
-Pool.Math.geoAvg = function(a, b) {
+Pool.Math.avgAG = function(a, b) {
 	if(!isNaN(Number(a)) && isFinite(Number(a)) && !isNaN(Number(b)) && isFinite(Number(b)))
 		return Math.sqrt(a * b);
 	else
@@ -1479,12 +1479,12 @@ Pool.Math.geoAvg = function(a, b) {
  * @param {Number} n - 자연수
  */
  Pool.Math.fct = function(n) {
-	if(Math.floor(n) == n && Math.abs(n) == n && isFinite(Number(n))) {
+	if(Math.floor(n) == n && Math.abs(n) == n && isFinite(Number(n)))
 		var sum = 1;
 		for(var e = n; e > 0; e--)
 			sum = sum * e;
 		return sum;
-	}else {
+	else {
 		throw new Error("Illegal argument type");
 	}
 };
