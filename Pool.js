781a082851afcaa7f1a6e3a17146659e763d09f7
CodeInside <scgtdy7151@gmail.com>
2015-03-07 20:49:48 +0900

    Server.broadcast(str)추가 - 방장이 서버원 모두에게 들리는 채팅 전송

--- Pool.js
+++ Pool.js
@@ -1539,6 +1539,33 @@ Pool.Math.ceilCut = function(a, b) {
 
 
 /**
+ * @since 2015-03-07 (API 1)
+ * @author CI-CodeInside <scgtdy7151@gmail.com>
+ * @namespace
+ */
+Pool.Server = {};
+
+/**
+ * 방장이 서버원 모두에게 들리는 채팅메시지를 보냅니다
+ * 
+ * @since 2015-03-07 (API 1)
+ * @author CI-CodeInside <scgtdy7151@gmail.com>
+ * @param {String} str
+ */
+Pool.Server.broadcast = function(str){
+	try{
+		clientMessage("<" + Player.getName(Player.getEntity()) + "> " + str);
+		net.zhuoweizhang.mcpelauncher.ScriptManager.nativeSendChat(str);
+	}catch(e){
+		Pool.showError(e);
+	}
+};
+
+
+
+
+
+/**
  * @since 2015-02-26 (API 1)
  * @author ChalkPE <amato0617@gmail.com>
  * @memberof Math
