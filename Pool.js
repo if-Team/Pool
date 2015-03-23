6884854fb21cb918034c4dbc068a7702807f9e73
우유맛비누 <milk5590@naver.com>
2015-03-23 22:52:30 +0900

    Update Pool.js

--- Pool.js
+++ Pool.js
@@ -1647,7 +1647,18 @@ Pool.Server.broadcast = function(str){
 };
 
 
+/**
+ * @since 2015-03-23 (API 1)
+ * @author 우유맛비누 <nno88551@naver.com>
+ * @param {Object} key
+ * @param {Object} value
+ */
+Pool.HashMap = function(key, value){
+	Object.defineProperty(this, "key", {value: key, writable: false});
+	Object.defineProperty(this, "value", {value: value, writable: false});
+};
 
+//사실 우유맛비누가 프로토타입 만들기 귀찮다네요 추후에 추가예정정
 
 
 /**
