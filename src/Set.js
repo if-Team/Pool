1b78b9cf4ed816929fc1366d1d4cca2e61d8cc3f
일가크 <woni8708@naver.com>
2015-06-19 23:46:26 +0900

    yeah!
    constructer!!!

--- src/Set.js
+++ src/Set.js
@@ -3,5 +3,9 @@
  * @author IchiKaku<woni8708@naver.com>
  * @namespace
  */
-
 Pool.Set = {};
+
+Pool.Set = function(ele) {
+  if(typeof ele !== "array") throw newError("parameter must be array");
+  this.elements = ele;
+};
