f058e05d06def85c347f29405b1d6255e622e573
일가크 <woni8708@naver.com>
2015-06-19 23:52:52 +0900

    add function getInter

--- src/Set.js
+++ src/Set.js
@@ -1,11 +1,36 @@
-/*
+/**
  * @since 2015-06-19
  * @author IchiKaku<woni8708@naver.com>
  * @namespace
  */
 Pool.Set = {};
 
+/**
+ * @since 2015-06-19
+ * @author IchiKaku<woni8708@naver.com>
+ * @param ele "array"
+ * @constructor
+ */
 Pool.Set = function(ele) {
-  if(typeof ele !== "array") throw newError("parameter must be array");
+  if(typeof ele !== "array") throw new Error("parameter must be array");
   this.elements = ele;
+};
+
+/**
+ * @since 2015-06-19
+ * @author IchiKaku<woni8708@naver.com>
+ * @param setA
+ * @param setB
+ */
+Pool.Set.getInter = function(setA, setB) {
+  var rst = [];
+  for(var i = 0; i < setA.length; i++) {
+    for(var m = 0; m < setB.length; m++) {
+      if(setA[i] == setB[m]) {
+        rst.push(setA[i]);
+        break;
+      }
+    }
+  }
+  return rst;
 };
